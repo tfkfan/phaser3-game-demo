3027132a73b8a9a9cd0d301666e0c9b0974c98ed
tfkfan <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>
2024-01-08 20:33:04 +0300

    added build

---
+++ last_build/assets/maps/new/objects.tsx
@@ -0,0 +1,69 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="objects" tilewidth="32" tileheight="32" tilecount="3072" columns="64">
+ <image source="C:/Users/PC1/Downloads/objects.png" width="2048" height="1536"/>
+ <tile id="1472">
+  <properties>
+   <property name="type" value="intelligence_book"/>
+  </properties>
+ </tile>
+ <tile id="1473">
+  <properties>
+   <property name="type" value="power_book"/>
+  </properties>
+ </tile>
+ <tile id="1474">
+  <properties>
+   <property name="type" value="mana_book"/>
+  </properties>
+ </tile>
+ <tile id="1475">
+  <properties>
+   <property name="type" value="magic_power_book"/>
+  </properties>
+ </tile>
+ <tile id="1476">
+  <properties>
+   <property name="type" value="xp_book"/>
+  </properties>
+ </tile>
+ <tile id="1477">
+  <properties>
+   <property name="type" value="speed_book"/>
+  </properties>
+ </tile>
+ <tile id="1478">
+  <properties>
+   <property name="type" value="health_book"/>
+  </properties>
+ </tile>
+ <tile id="1600">
+  <properties>
+   <property name="type" value="xp_potion"/>
+  </properties>
+ </tile>
+ <tile id="1601">
+  <properties>
+   <property name="type" value="no_effect_potion"/>
+  </properties>
+ </tile>
+ <tile id="1602">
+  <properties>
+   <property name="type" value="health_potion"/>
+  </properties>
+ </tile>
+ <tile id="1603">
+  <properties>
+   <property name="type" value="death_potion"/>
+  </properties>
+ </tile>
+ <tile id="1604">
+  <properties>
+   <property name="type" value="speed_potion"/>
+  </properties>
+ </tile>
+ <tile id="1605">
+  <properties>
+   <property name="type" value="magic_power_potion"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ last_build/assets/maps/new/tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="tiles+" tilewidth="32" tileheight="32" spacing="1" tilecount="380" columns="19">
+ <image source="tiles.png" trans="ffffff" width="626" height="659"/>
+</tileset>
